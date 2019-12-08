63781d85fd8fdce7d664a1e771651ac21d2ec74f
Minami Munakata <june28munazo@gmail.com>
2019-12-08 17:23:40 +0900

    Modify SearchActivity

--- app/src/main/java/com/raywenderlich/android/droidwiki/dagger/AppComponent.kt
+++ app/src/main/java/com/raywenderlich/android/droidwiki/dagger/AppComponent.kt
@@ -1,6 +1,7 @@
 package com.raywenderlich.android.droidwiki.dagger
 
 import com.raywenderlich.android.droidwiki.ui.homepage.HomepageActivity
+import com.raywenderlich.android.droidwiki.ui.search.SearchActivity
 import dagger.Component
 import javax.inject.Singleton
 
@@ -8,6 +9,7 @@ import javax.inject.Singleton
 @Component(modules = [AppModule::class, PresenterModule::class, NetworkModule::class, WikiModule::class])
 interface AppComponent {
     fun inject(target: HomepageActivity)
+    fun inject(target: SearchActivity)
 }
 
 
